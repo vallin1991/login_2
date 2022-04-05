4c380b55e147757ad42411d8a7553d2c7c91a4ce
vallin1991 <victorvallinavila@gmail.com>
2022-04-04 21:21:18 -0700

    created the pages,components & assests directorys

---
+++ src/components/Styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+//background
+export const colors ={
+    primary: "#fff",
+    theme: "#BE185D",
+    light1: "#F3F4F6",
+    light2: "#ESE7EB",
+    dark1: "#1F2937",
+    dark2: "#4B5563",
+    dark3: "#9CA3AF",
+    red: "#DC2626"
+}
